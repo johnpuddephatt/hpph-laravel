16695ef8c8f7356b71c38c6835c5e84ea41516ce
John Puddephatt <john@jdp.org.uk>
2018-04-27 23:59:35 +0100

    install backup manager

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,2 +1,3 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('film') }}"><i class="fa fa-film"></i> <span>Films</span></a></li>
+<li><a href='{{ url(config('backpack.base.route_prefix', 'admin').'/backup') }}'><i class='fa fa-hdd-o'></i> <span>Backups</span></a></li>
