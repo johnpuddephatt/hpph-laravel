c7ea51e31b5e84259186977a2fe2c801fcac89e8
John Puddephatt <john@jdp.org.uk>
2018-08-07 15:48:13 +0100

    dynamic label colours fix error

--- resources/views/film/strand.blade.php
+++ resources/views/film/strand.blade.php
@@ -1,5 +1,5 @@
 <span style="background-color: {{ $strand->color }};
-@if((hexdec(substr($strand->color,0,2)) + hexdec(substr($hex,2,2)) + hexdec(substr($hex,4,2))) < 250 )
+@if((hexdec(substr($strand->color,0,2)) + hexdec(substr($strand->color,2,2)) + hexdec(substr($strand->color,4,2))) < 250 )
  color: #ffffff;
 @endif
 " title="{{ $strand->description }}" class="label label--{{ str_slug($strand->title) }}">{{ $strand->title }}</span>
