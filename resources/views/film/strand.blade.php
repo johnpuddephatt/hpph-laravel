ed030a9079030ccaf8efe0c668ffc10fbb713cfa
John Puddephatt <john@jdp.org.uk>
2018-08-07 15:47:21 +0100

    dynamic label colours

--- resources/views/film/strand.blade.php
+++ resources/views/film/strand.blade.php
@@ -1,1 +1,5 @@
-<span title="{{ $strand->description }}" class="label label--{{ str_slug($strand->title) }}">{{ $strand->title }}</span>
+<span style="background-color: {{ $strand->color }};
+@if((hexdec(substr($strand->color,0,2)) + hexdec(substr($hex,2,2)) + hexdec(substr($hex,4,2))) < 250 )
+ color: #ffffff;
+@endif
+" title="{{ $strand->description }}" class="label label--{{ str_slug($strand->title) }}">{{ $strand->title }}</span>
