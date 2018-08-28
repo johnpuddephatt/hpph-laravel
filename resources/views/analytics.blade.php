6ee0e4fa0393519d18acc0368ef82099de73fcff
John Puddephatt <john@jdp.org.uk>
2018-08-28 19:54:15 +0100

    Edge 17 footer fixes

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-48108307-1"></script>
+<script type="text/javascript" async src="https://www.googletagmanager.com/gtag/js?id=UA-48108307-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+      <meta charset="utf-8">
+
       @if(env('GOOGLE_ANALYTICS'))
         @include('analytics')
       @endif
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>{{ env('APP_NAME')}}</title>
       <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
@@ -20,7 +21,7 @@
 
     @include('footer')
 
-    <script src="{{ mix('/js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
 
   </body>
 </html>
