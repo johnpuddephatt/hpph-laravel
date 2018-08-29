bc479d39f7bf745aa820b752fb89d419ed6d9a3d
John Puddephatt <john@jdp.org.uk>
2018-08-29 17:24:54 +0100

    update footer links

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -36,11 +36,11 @@
         <h2 class="site-footer--header sr-only">Learn more</h2>
 
         <ul class="site-footer--menu-links">
-          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="#">Jobs &amp; volunteering</a></li>
-          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="#">Accessibility</a></li>
-          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="#">Press</a></li>
-          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="#">Terms &amp; conditions</a></li>
-          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="#">Privacy policy</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/jobs">Jobs &amp; volunteering</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/accessibility">Accessibility</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/press">Press</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/terms">Terms &amp; conditions</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/privacy">Privacy policy</a></li>
         </ul>
       </div>
       <div class="site-footer--social">
