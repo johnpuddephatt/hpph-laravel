b9baeea54fbd9c62009cdc1ec69bfdae88c251fe
John Puddephatt <john@jdp.org.uk>
2018-08-30 19:22:35 +0100

    space alert on pages

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -40,7 +40,7 @@
         <ul class="site-footer--menu-links">
           <li class="site-footer--menu-item"><a class="site-footer--menu-link hide-on-desktop" href="https://hydeparkpicturehouse.bigcartel.com/">Shop</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link hide-on-desktop" href="https://friendsofhpph.org/join/">Membership</a></li>
-          {{-- <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/jobs">Jobs &amp; volunteering</a></li>
+          {{--<li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/jobs">Jobs &amp; volunteering</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/accessibility">Accessibility</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/press">Press</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/terms">Terms &amp; conditions</a></li>
