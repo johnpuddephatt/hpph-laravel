4cd3c41f8b6069536dbe3fe5b596df0c59211b43
John Puddephatt <john@jdp.org.uk>
2018-09-05 01:31:36 +0100

    show contact link on desktop

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -37,8 +37,8 @@
         <h2 class="site-footer--header sr-only">Learn more</h2>
         <ul class="site-footer--menu-links">
           <li class="site-footer--menu-item hide-on-desktop"><a class="site-footer--menu-link" href="https://hydeparkpicturehouse.bigcartel.com/">Shop</a></li>
-          <li class="site-footer--menu-item hide-on-desktop"><a class="site-footer--menu-link" href="/membership/">Membership</a></li>
-          <li class="site-footer--menu-item hide-on-desktop"><a class="site-footer--menu-link" href="/contact/">Contact</a></li>
+          <li class="site-footer--menu-item hide-on-desktop"><a class="site-footer--menu-link" href="/membership">Membership</a></li>
+          <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/contact">Contact</a></li>
           {{--<li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/jobs">Jobs &amp; volunteering</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/accessibility">Accessibility</a></li>
           <li class="site-footer--menu-item"><a class="site-footer--menu-link" href="/press">Press</a></li>
