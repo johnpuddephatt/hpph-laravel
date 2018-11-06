fdc771c10de1407e4b07f7f433bc1b60a077e0f7
John Puddephatt <john@jdp.org.uk>
2018-11-06 02:20:58 +0000

    dont show empty reviews

--- resources/views/film/single.blade.php
+++ resources/views/film/single.blade.php
@@ -69,23 +69,25 @@
       <div class="single-listing--reviews">
         @if($film->reviews)
           @foreach($film->reviews as $review)
-            <div class="single-listing--reviews--review">
-            @if(!empty($review->url))<a href="{{ $review->url }}" _target="blank">@endif
-            @if(!empty($review->text))<div class="single-listing--reviews--review--text">“{{ $review->text }}”</div>@endif
             @if(!empty($review->rating))
-              <div class="single-listing--reviews--review--rating">
-                @for ($i = 0; $i < $review->rating; $i++)
-                  &starf;
-                @endfor
+              <div class="single-listing--reviews--review">
+              @if(!empty($review->url))<a href="{{ $review->url }}" _target="blank">@endif
+              @if(!empty($review->text))<div class="single-listing--reviews--review--text">“{{ $review->text }}”</div>@endif
+              @if(!empty($review->rating))
+                <div class="single-listing--reviews--review--rating">
+                  @for ($i = 0; $i < $review->rating; $i++)
+                    &starf;
+                  @endfor
+                </div>
+              @endif
+              @if(!empty($review->author))
+                <div class="single-listing--reviews--review--author">
+                  {{ $review->author }}
+                </div>
+              @endif
+              @if(!empty($review->url))</a>@endif
               </div>
             @endif
-            @if(!empty($review->author))
-              <div class="single-listing--reviews--review--author">
-                {{ $review->author }}
-              </div>
-            @endif
-            @if(!empty($review->url))</a>@endif
-            </div>
           @endforeach
         @endif
       </div>
