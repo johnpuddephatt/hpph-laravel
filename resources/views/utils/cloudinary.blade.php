59b09d8c6c7298ca4ee8093ba3278ff0c1b5e6d7
John Puddephatt <john@jdp.org.uk>
2018-11-08 13:02:55 +0000

    drop cloudinary Q to 75

--- resources/views/utils/cloudinary.blade.php
+++ resources/views/utils/cloudinary.blade.php
@@ -4,9 +4,9 @@ $breakpoints = [90,120,150,180,240,360,480,640,720,960,1080,1200,1440,1600]
 
 @if (config('app.env') != 'local')
   @if (isset($pathonly) && $pathonly == true)
-    https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? 80 }},{{ $modes ?? "c_fill,g_auto,f_auto" }}/{{ url($img) }}
+    https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? 75 }},{{ $modes ?? "c_fill,g_auto,f_auto" }}/{{ url($img) }}
   @else
-    <img alt="{{ $alt }}" src="https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? 75 }},{{ $modes ?? 'c_fill,g_auto,f_auto' }}/{{ url($img) }}" width="{{ $width ?? 500 }}" height="{{ $height ?? 500 }}" srcset="@foreach ($breakpoints as $breakpoint) https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $breakpoint }},h_{{ round($height * $breakpoint / $width) }},q_{{ $quality ?? 80 }},{{ $modes ?? 'c_fill,g_auto,f_auto' }}/{{ url($img) }} {{ $breakpoint }}w @if (!$loop->last), @endif @endforeach" sizes="{{ $sizes ?? "100vw" }}" class="{{ $class ?? '' }}" @if(isset($onload)) onload="{{ $onload }}" @endif />
+    <img alt="{{ $alt }}" src="https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? 75 }},{{ $modes ?? 'c_fill,g_auto,f_auto' }}/{{ url($img) }}" width="{{ $width ?? 500 }}" height="{{ $height ?? 500 }}" srcset="@foreach ($breakpoints as $breakpoint) https://res.cloudinary.com/{{ config('app.cloudinary') }}/image/fetch/w_{{ $breakpoint }},h_{{ round($height * $breakpoint / $width) }},q_{{ $quality ?? 75 }},{{ $modes ?? 'c_fill,g_auto,f_auto' }}/{{ url($img) }} {{ $breakpoint }}w @if (!$loop->last), @endif @endforeach" sizes="{{ $sizes ?? "100vw" }}" class="{{ $class ?? '' }}" @if(isset($onload)) onload="{{ $onload }}" @endif />
   @endif
 @else
   @if (isset($pathonly) && $pathonly == true)
