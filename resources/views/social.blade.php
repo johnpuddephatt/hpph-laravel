abc5400f1d60b8ad2d2bdfc4f4bf9b4a217166e8
John Puddephatt <john@jdp.org.uk>
2018-11-08 12:25:44 +0000

    add meta desc

--- resources/views/social.blade.php
+++ resources/views/social.blade.php
@@ -1,6 +1,6 @@
 <ul class="header--social">
-  @if(config('app.facebook'))<li><a title="Facebook" href="//facebook.com/{{ config('app.facebook') }}" target="_blank">@include('icons.facebook') <span>Facebook</span></a></li>@endif
-  @if(config('app.instagram'))<li><a title="Instagram" href="//instagram.com/{{ config('app.instagram') }}" target="_blank">@include('icons.instagram')  <span>Instagram</span></a></li>@endif
-  @if(config('app.twitter'))<li><a title="Twitter" href="//twitter.com/{{ config('app.twitter') }}" target="_blank">@include('icons.twitter')  <span>Twitter</span></a></li>@endif
-  @if(config('app.flickr'))<li><a title="Flickr" href="//flickr.com/photos/{{ config('app.flickr') }}" target="_blank">@include('icons.flickr')  <span>Flickr</span></a></li>@endif
+  @if(config('app.facebook'))<li><a title="Facebook" rel="noopener" href="//facebook.com/{{ config('app.facebook') }}" target="_blank">@include('icons.facebook') <span>Facebook</span></a></li>@endif
+  @if(config('app.instagram'))<li><a title="Instagram" rel="noopener" href="//instagram.com/{{ config('app.instagram') }}" target="_blank">@include('icons.instagram')  <span>Instagram</span></a></li>@endif
+  @if(config('app.twitter'))<li><a title="Twitter" rel="noopener" href="//twitter.com/{{ config('app.twitter') }}" target="_blank">@include('icons.twitter')  <span>Twitter</span></a></li>@endif
+  @if(config('app.flickr'))<li><a title="Flickr" rel="noopener" href="//flickr.com/photos/{{ config('app.flickr') }}" target="_blank">@include('icons.flickr')  <span>Flickr</span></a></li>@endif
 </ul>
