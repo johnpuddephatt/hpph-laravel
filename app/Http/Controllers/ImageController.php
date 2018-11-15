86bc6270e06207008e6896dfef5766445b5708c2
John Puddephatt <john@jdp.org.uk>
2018-11-15 12:39:58 +0000

    tweaking imager code

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -15,48 +15,64 @@ class ImageController extends Controller
 {
   public function default($config_string,$image) {
 
+    // Load config
     $a = explode(',', $config_string);
     foreach ($a as $result) {
         $b = explode('_', $result);
         $config[$b[0]] = $b[1];
     }
 
+    // Format
     $config['f'] = pathinfo($image, PATHINFO_EXTENSION);
-
     if ( !in_array($config['f'], array('jpg','jpeg','png','webp'), true ) ) abort('404');
+    if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) $config['f'] = 'webp';
 
+    // Gravity
+    $config['g'] = isset($config['g']) ? $config['g'] : null;
+    // Height and width
+    $config['w'] = isset($config['w']) ? $config['w'] : null;
+    $config['h'] = isset($config['h']) ? $config['h'] : null;
+    // Quality
     if(!isset($config['q'])) {
       $config['q'] = config('imagecache.quality');
     }
 
+    //   if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) {
+
 
     $src = url($image);
-    $img = Image::cache(function($image) use ($src,$config) {
 
+    $img = Image::cache(function($image) use($src,$config) {
+      $image->make($src)->fit($config['w'], $config['h'], null, $config['g'])->encode($config['f'], $config['q']);
+    }, 10, false);
 
-      $image->make($src);
-
-      // Resize or crop
-      if(isset($config['w']) && isset($config['h'])) {
-        $g = isset($config['g']) ? $config['g'] : null;
-        $image->fit($config['w'], $config['h'],null,$g);
-      }
-      elseif (isset($config['w'])) {
-        $image->widen($config['w']);
-      }
-      elseif (isset($config['h'])) {
-        $image->heighten($config['h']);
-      }
-
-      // Size and format
-
-      if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) {
-        $image->encode('webp', $config['q']);
-      }
-      else {
-        $image->encode($config['f'], $config['q']);
-      }
-    });
+    // $img = Image::cache(function($image) use ($src,$config) {
+    //
+    //   $image->make($src);
+    //
+    //   // Resize or crop
+    //   if(isset($config['w']) && isset($config['h'])) {
+    //     $image->fit($config['w'], $config['h'],null,$config['g']);
+    //   }
+    //   elseif (isset($config['w'])) {
+    //     $image->widen($config['w']);
+    //   }
+    //   elseif (isset($config['h'])) {
+    //     $image->heighten($config['h']);
+    //   }
+    //
+    //   // Size and format
+    //   if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) {
+    //     $image->encode('webp', $config['q']);
+    //     echo 'hello';
+    //     return $image;
+    //   }
+    //   else {
+    //     $image->encode($config['f'], $config['q']);
+    //     return $image;
+    //     // return $image;
+    //   }
+    // });
 
     if( strpos( $_SERVER['HTTP_ACCEPT'], 'image/webp' ) !== false ) {
       return Response::make($img, 200, array('Content-Type' => 'image/webp'));
@@ -67,7 +83,6 @@ class ImageController extends Controller
     elseif($config['f'] == 'png') {
       return Response::make($img, 200, array('Content-Type' => 'image/png'));
     }
-
 
   }
 }
