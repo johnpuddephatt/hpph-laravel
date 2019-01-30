3ad834dedfe8ceb2b72c7c58d5022b3db1ad3508
John Puddephatt <john@jdp.org.uk>
2019-01-30 15:09:47 +0000

    add preceding slash to intervention 2

--- resources/views/utils/intervention.blade.php
+++ resources/views/utils/intervention.blade.php
@@ -3,7 +3,7 @@ $breakpoints = isset($breakpoints) ? $breakpoints : [90,120,150,180,240,360,480,
 @endphp
 
 <img alt="{{ $alt }}"
-  src="/imager/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? config('imagecache.quality') }},f_jpg,g_center{{ $img }}"
+  src="/imager/w_{{ $width ?? 500 }},h_{{ $height ?? 500 }},q_{{ $quality ?? config('imagecache.quality') }},f_jpg,g_center/{{ $img }}"
   width="{{ $width ?? 500 }}"
   height="{{ $height ?? 500 }}"
   srcset="@foreach ($breakpoints as $breakpoint) /imager/w_{{ $breakpoint }},h_{{ round($height * $breakpoint / $width) }},q_{{ $quality ?? config('imagecache.quality') }},f_jpg,g_center/{{ $img }} {{ $breakpoint }}w @if (!$loop->last), @endif @endforeach" sizes="{{ $sizes ?? "100vw" }}" class="{{ $class ?? '' }}" @if(isset($onload)) onload="{{ $onload }}" @endif />
