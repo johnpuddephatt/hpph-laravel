b1f7ab2478f6f5d9936fd010b26156c447b14d9f
John Puddephatt <john@jdp.org.uk>
2019-09-30 14:35:16 +0100

    experiment with navbar on ios

--- resources/js/app.js
+++ resources/js/app.js
@@ -467,7 +467,9 @@ if(iOSSafari || iOSChrome) {
     // document.documentElement.classList.add('iOSSafari');
     var homeSlider = document.querySelector('.section--home-slider');
     if(homeSlider) {
-      homeSlider.style.height = document.documentElement.clientHeight - (navBar.clientHeight/2) + 1 + 'px';
+      // homeSlider.style.height = document.documentElement.clientHeight - navBar.clientHeight + 1 + 'px';
+      homeSlider.style.height = document.documentElement.clientHeight;
+      homeSlider.classList.add('ios');
     }
   });
 }
