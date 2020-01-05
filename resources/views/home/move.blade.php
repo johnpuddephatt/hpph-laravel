47ab3b74e3893f77df4b117bb4574d6515aa585e
John Puddephatt <john@jdp.org.uk>
2020-01-05 20:19:46 +0000

    update closure message

--- resources/views/home/move.blade.php
+++ resources/views/home/move.blade.php
@@ -5,9 +5,9 @@
   <div class="moving--band">
     <div class="container">
       <div class="moving--text">
-      <p>From January 2020 work will begin on The Picture House Project, restoring and renewing our historic cinema for all.</p>
+      <p>From February 2020 work will begin on The Picture House Project, restoring and renewing our historic cinema for all.</p>
       <p>For twelve months we’ll be taking to the road, delivering an exciting programme of films and special events across the city.</p>
-      <p>Details of next year’s plans will be available early November.</p>
+      <p>Details of next year’s plans will be available early January.</p>
       <a target="_blank" href="https://thepicturehouseproject.com">Find out more about The Picture House Project »</a>
       </div>
       <div class="moving--image">
