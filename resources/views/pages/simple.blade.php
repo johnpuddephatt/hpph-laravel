41448c141f9883a2537b97ecc24f30bf0c738f84
John Puddephatt <john@jdp.org.uk>
2020-03-30 11:08:20 +0100

    check for iframe property

--- resources/views/pages/simple.blade.php
+++ resources/views/pages/simple.blade.php
@@ -10,7 +10,7 @@
     <main class="page-standard--main page-standard--main__simple">
       {!! $page->content !!}
 
-      @if(property_exists(json_decode($page->extras)->iframe))
+      @if(property_exists(json_decode($page->extras), 'iframe'))
         <iframe src="https://tickets.hydeparkpicturehouse.co.uk/hydeparkpicturehouse/website/{{ json_decode($page->extras)->iframe }}.aspx?resize=true{{ json_decode($page->extras)->iframe_parameters ? '&' . str_replace(' ', '%20', json_decode($page->extras)->iframe_parameters) : '' }}" class="spectrix-iframe spectrix-{{ json_decode($page->extras)->iframe }}" id="SpektrixIFrame" name="SpektrixIFrame"></iframe>
         <script type="text/javascript" src="https://tickets.hydeparkpicturehouse.co.uk/hydeparkpicturehouse/website/scripts/integrate.js"></script>
       @endif
