831f861ab3c1f2681947793d11f2dd9576d7f458
John Puddephatt <john@jdp.org.uk>
2020-04-08 21:56:06 +0100

    css notificaiton

--- resources/views/notification.blade.php
+++ resources/views/notification.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     {{ env('SITE_NOTIFICATION') }}
     @if(env('SITE_NOTIFICATION_URL'))
-      <a class="button button__gray" href="{{ env('SITE_NOTIFICATION_URL') }}">Find out more</a>
+      <a class="button button__gray" href="{{ env('SITE_NOTIFICATION_URL') }}">Learn more</a>
     @endif
   </div>
 </div>
