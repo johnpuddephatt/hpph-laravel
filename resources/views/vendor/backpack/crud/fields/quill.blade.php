7f2466afec221ddfaf91cebc071b5cec4c93d569
John Puddephatt <john@jdp.org.uk>
2020-07-08 12:31:13 +0100

    no

--- resources/views/vendor/backpack/crud/fields/quill.blade.php
+++ resources/views/vendor/backpack/crud/fields/quill.blade.php
@@ -181,7 +181,7 @@
 
   if(typeof DividerBlot === 'undefined') {
     let BlockEmbed = Quill.import('blots/block/embed');
-    class DividerBlot extends BlockEmbed;
+    class DividerBlot extends BlockEmbed {};
     DividerBlot.blotName = 'divider';
     DividerBlot.tagName = 'hr';
     Quill.register(DividerBlot);
