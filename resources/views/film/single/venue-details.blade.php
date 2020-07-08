879ad5e63809d5b1b166d1607fa0291b45177165
John Puddephatt <john@jdp.org.uk>
2020-07-08 11:25:47 +0100

    hide venue details link if nothing to show

--- resources/views/film/single/venue-details.blade.php
+++ resources/views/film/single/venue-details.blade.php
@@ -3,31 +3,34 @@
     <h2 class="single-listing--venue--header is-closable">Showing at</h2>
     <p class="single-listing--venue--title">{{$film->venue->title}}</p>
 
-    <details class="single-listing--venue--details">
-        <summary>Venue details</summary>
-        @if($film->venue->address)
-          <h3>Address</h3>
-          <p>
-            {{$film->venue->address->value}}<br>
-            <a href="https://maps.google.com/?q={{ urlencode($film->venue->address->value) }}" target="_blank">View on map</a>
-          </p>
-        @endif
+    @if($film->venue->address || $film->venue->access_info || $film->venue->refreshment_info || $film->venue->parking_info)
 
-        @if($film->venue->access_info)
-          <h3>Access info</h3>
-          <p>{!! $film->venue->access_info !!}</p>
-        @endif
+      <details class="single-listing--venue--details">
+          <summary>Venue details</summary>
+          @if($film->venue->address)
+            <h3>Address</h3>
+            <p>
+              {{$film->venue->address->value}}<br>
+              <a href="https://maps.google.com/?q={{ urlencode($film->venue->address->value) }}" target="_blank">View on map</a>
+            </p>
+          @endif
 
-        @if($film->venue->refreshment_info)
-          <h3>Refreshment info</h3>
-          <p>{!! $film->venue->refreshment_info !!}</p>
-        @endif
+          @if($film->venue->access_info)
+            <h3>Access info</h3>
+            <p>{!! $film->venue->access_info !!}</p>
+          @endif
 
-        @if($film->venue->parking_info)
-          <h3>Parking info</h3>
-          <p>{!! $film->venue->parking_info !!}</p>
-        @endif
+          @if($film->venue->refreshment_info)
+            <h3>Refreshment info</h3>
+            <p>{!! $film->venue->refreshment_info !!}</p>
+          @endif
 
-    </details>
+          @if($film->venue->parking_info)
+            <h3>Parking info</h3>
+            <p>{!! $film->venue->parking_info !!}</p>
+          @endif
+
+      </details>
+    @endif
   </div>
 @endif
