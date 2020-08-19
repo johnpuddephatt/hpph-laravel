c90ac9306a7b8b2ea7ee918a37e6fb2553f187aa
John Puddephatt <john@jdp.org.uk>
2020-08-19 13:38:18 +0100

    lost banner copy update

--- resources/views/home/lost.blade.php
+++ resources/views/home/lost.blade.php
@@ -5,7 +5,7 @@
   <div class="lost--band">
     <div class="container">
       <div class="lost--text">
-      <p>Hiding in Plain Sight – our exciting new heritage project, produced in collaboration with AAAB Illustration – is now live!<p>
+      <p>Hiding in Plain Sight – our exciting new heritage project, produced in collaboration with illustrator Adam Allsuch Boardman – is now live!<p>
       <p>Our new interactive microsite allows users to navigate the city and visit the locations of dozens of past cinemas, many of which have survived and are still visible today.</p>
       <a target="_blank" href="https://lostcinemas.co.uk">Discover the lost cinemas of Leeds »</a>
       </div>
