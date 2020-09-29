b42d158aa5132d1d2a7fbab47678183cdb280932
John Puddephatt <john@jdp.org.uk>
2020-09-29 20:21:00 +0100

    move digi releases

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,8 +1,8 @@
 <ul class="site-header--navigation">
   {{-- <li><a href="/strand/digital-releases">What’s on</a></li> --}}
   <li><a href="/whats-on">What’s on</a></li>
-  <li><a href="/support/">Support us</a></li>
   <li><a href="/strand/digital-releases">Digital releases</a></li>
+  <li><a href="/support/">Support us</a></li>
   {{-- <li><a href="/picks/">Hyde Park Picks</a></li> --}}
   <li><a href="https://leedsheritagetheatres.bigcartel.com/category/hyde-park-picture-house">Shop</a></li>
   <li><a href="/membership/">Membership</a></li>
