580ab5e1108e8607f84c1239dfb292022a056775
John Puddephatt <john@jdp.org.uk>
2021-10-01 19:53:37 +0100

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
   public function index($day = 1) {
 
     $home_hydeandseek = \Cache::rememberForever('homeHydeSeek', function () {
-      // Strand::where('slug', 'hyde-seek')->first()->screenings()->first()->film()->firstOrFail();
+      return App\Models\Strand::where('slug','hyde-seek')->first()->films()->latest()->first();
     });
 
     // $home_online = \Cache::rememberForever('homeOnline', function () {
