4e47ef3cae2abc36ba0abcfd3a59be080ec89c37
John Puddephatt <john@jdp.org.uk>
2021-11-02 22:31:39 +0000

    mobile basket

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -6,7 +6,7 @@
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
     </svg>
-    <div clas="basket-text">
+    <div class="basket-text">
       <span data-basket-item-count></span> item(s) (<span data-basket-summary-currency></span><span
         data-basket-summary-basket-total></span>)
     </div>
