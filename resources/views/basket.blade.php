5a4160bba7d6a1698b29fd47ace7a1e7367e40b7
John Puddephatt <john@jdp.org.uk>
2021-11-02 09:13:35 +0000

    update basket summary to link to basket not checkout

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -1,7 +1,7 @@
 @if(config('app.env') === 'production')
 <spektrix-basket-summary hidden id="spektrixBasketSummary" client-name="hydeparkpicturehouse"
   custom-domain="tickets.hydeparkpicturehouse.co.uk">
-  <a href="/checkout/">
+  <a href="/basket/">
     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
         d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
