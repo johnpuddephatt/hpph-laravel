4ab6cb614c830570afdc2719107f6e38f6fb26b5
John Puddephatt <john@jdp.org.uk>
2021-11-16 17:13:23 +0000

    home hero

--- resources/views/home/donate_hero.blade.php
+++ resources/views/home/donate_hero.blade.php
@@ -1,7 +1,7 @@
 <header
   class="min-h-[100vw] md:min-h-[50vw] items-center flex px-6 lg:px-32 space-x-8 bg-christmas-beige border-solid border-0 border-b relative">
   <div class="container w-full px-0 mb-32 md:mb-24">
-    <div class="w-5/6">
+    <div class="relative w-5/6">
       <div class="mb-2 font-serif text-4xl italic font-normal lg:text-5xl text-christmas-blue">Winter Fundraiser
       </div>
       <h1 class="font-serif text-5xl italic font-normal lg:text-6xl">Help us deliver vital conservation work this
@@ -10,6 +10,6 @@
         more</a>
     </div>
     <img src="{{ @asset('/images/winter-fundraising-banner.svg') }}"
-      class="absolute right-0 w-full md:w-[80%] bottom-12">
+      class="absolute pointer-events-none right-0 w-full md:w-[80%] bottom-12">
   </div>
 </header>
