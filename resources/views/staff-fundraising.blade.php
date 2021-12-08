02b1e12402ea7e41eee152ae672f08801545a52e
John Puddephatt <john@jdp.org.uk>
2021-12-08 14:01:47 +0000

    line breaks

--- resources/views/staff-fundraising.blade.php
+++ resources/views/staff-fundraising.blade.php
@@ -9,7 +9,7 @@
     <br><br><br><br>
     <spektrix-donate donation-amount="50" id="spektrixDonate" client-name="hydeparkpicturehouse"
         custom-domain="tickets.hydeparkpicturehouse.co.uk" fund-id="1201AQKTLDJVPVNVGVNTLHSSDPLSGRGBT">
-        <h3>Sponsor a seat – staff and volunteers</h3>
+        <h2>Sponsor a seat – staff and volunteers</h2>
         <p><strong>Seat sponsorship is open to the public for £100, but to say thank you to our staff and
                 volunteers we’re making this available to you at a reduced rate of £50.</strong></p>
         <p>As a thank you for sponsoring a seat, we’ll install a personalised plaque on a seat of your choice, in either
@@ -18,6 +18,7 @@
         <input style="display: none;" id="tribute-name" type="text" data-tribute-name />
         <input style="display: none;" id="tribute-type" type="text" name="tributeType" data-tribute-type />
         <button class="button button__big" data-submit-donation>Add to basket</button>
+        <br>
         <div data-success-container style="display: none;">
             Added to basket. <a href="/checkout">Go to checkout</a>
         </div>
