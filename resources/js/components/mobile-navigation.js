0de0e7c585747c9be3946e69cbd4da21dee6e095
John Puddephatt <john@jdp.org.uk>
2022-01-03 22:36:16 +0000

    resize slideshow to not include navbar

--- resources/js/components/mobile-navigation.js
+++ resources/js/components/mobile-navigation.js
@@ -1,12 +1,12 @@
 /*
-** Open 'more' menu
-*/
+ ** Open 'more' menu
+ */
 
 var navTrigger = document.getElementById('nav-trigger');
 var nav = document.querySelector('.site-footer');
 var navBar = document.querySelector('.site-footer--navigation');
-if(navTrigger) {
-  navTrigger.addEventListener('click',(e)=>{
+if (navTrigger) {
+  navTrigger.addEventListener('click', e => {
     e.preventDefault();
     nav.classList.toggle('visible');
     navTrigger.classList.toggle('open');
@@ -17,8 +17,8 @@ if(navTrigger) {
 }
 
 /*
-** UA sniffing to prevent judder due to navbar movement on iOS
-*/
+ ** UA sniffing to prevent judder due to navbar movement on iOS
+ */
 
 var ua = window.navigator.userAgent;
 var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
@@ -26,13 +26,14 @@ var webkit = !!ua.match(/WebKit/i);
 var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 var iOSChrome = iOS && webkit && ua.match(/CriOS/i);
 
-if(iOSSafari || iOSChrome) {
-  document.addEventListener('DOMContentLoaded', ()=>{
+if (iOSSafari || iOSChrome) {
+  document.addEventListener('DOMContentLoaded', () => {
     // document.documentElement.classList.add('iOSSafari');
     var homeSlider = document.querySelector('.section--home-slider');
-    if(homeSlider) {
-      // homeSlider.style.height = document.documentElement.clientHeight - navBar.clientHeight + 1 + 'px';
-      homeSlider.style.height = document.documentElement.clientHeight;
+    if (homeSlider) {
+      homeSlider.style.height =
+        document.documentElement.clientHeight - navBar.clientHeight + 1 + 'px';
+      // homeSlider.style.height = document.documentElement.clientHeight;
       homeSlider.classList.add('ios');
     }
   });
