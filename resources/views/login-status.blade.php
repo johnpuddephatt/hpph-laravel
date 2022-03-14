4c48c27e381cededbc56a10002bb5341ead01186
John Puddephatt <john@jdp.org.uk>
2022-03-14 16:15:43 +0530

    account icon

--- resources/views/login-status.blade.php
+++ resources/views/login-status.blade.php
@@ -1,11 +1,48 @@
 <spektrix-login-status
   client-name="hydeparkpicturehouse"
   custom-domain="tickets.hydeparkpicturehouse.co.uk"
+  id="login-status"
 >
   <span data-logged-in-container style="display: none;">
-    <a href="/account">My account</a>
+    <a href="/account">
+      <span>
+        My account
+      </span>
+      <svg
+        aria-label="My account"
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-6 h-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </a>
   </span>
   <span data-logged-out-container>
-    <a href="/account">Login/Register</a>
+    <a href="/account">
+      <span>Login/Register</span>
+      <svg
+        aria-label="Login/Register"
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-6 h-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </a>
   </span>
 </spektrix-login-status>
