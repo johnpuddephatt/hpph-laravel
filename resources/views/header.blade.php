a3a7d26f802bf86ebe66c267cf87f0882b604630
John Puddephatt <john@jdp.org.uk>
2022-03-14 11:40:45 +0530

    reposition header left spacer

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,8 @@
 <header class="site-header">
   <div class="container">
+    <div class="site-header--left">
+      <!-- Intentionally empty, used as spacer on mobile -->
+    </div>
     <a href="{{ url('/') }}" class="site-header--home-link">
       <span class="sr-only">Hyde Park Picture House</span>
       @include('icons.logo-strapline')
--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,7 +1,3 @@
-<div class="site-header--left">
-  <!-- Intentionally empty, used as spacer on mobile -->
-</div>
-
 <ul class="site-header--navigation">
   <li><a href="/whats-on">What’s on</a></li>
   <li><a href="/support/">Support us</a></li>
