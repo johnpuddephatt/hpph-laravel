d8b9c2ad2daedec9dd12c7d9c040982faa3e128d
John Puddephatt <john@jdp.org.uk>
2022-04-08 18:02:04 +0100

    remove mailchimp links

--- resources/views/film/single/screening-details.blade.php
+++ resources/views/film/single/screening-details.blade.php
@@ -1,28 +1,31 @@
 <div class="single-listing--screenings">
-  <h2 class="single-listing--screenings--header @if(!$film->venue)is-closable @endif">Showtimes</h2>
+  <h2
+    class="single-listing--screenings--header @if(!$film->venue)is-closable @endif"
+  >
+    Showtimes
+  </h2>
 
   @if($film->screenings->contains(function($key, $value) { return $key->url; }))
-    <p class="screenings-table--explainer">Select a screening below to book tickets</p>
-  @endif
-
-  @if (count($film->screenings))
-    @include('screening.selector')
-
-  @elseif ($film->screenings_count)
-    <div class="alert alert__empty">
-      All scheduled screenings have now passed.
-    </div>
+  <p class="screenings-table--explainer">
+    Select a screening below to book tickets
+  </p>
+  @endif @if (count($film->screenings)) @include('screening.selector') @elseif
+  ($film->screenings_count)
+  <div class="alert alert__empty">
+    All scheduled screenings have now passed.
+  </div>
 
   @else
-    <div class="alert">
-      <h3>{{ $film->custom_coming_soon ?? "Showtimes to be confirmed" }}</h3>
+  <div class="alert">
+    <h3>{{ $film->custom_coming_soon ?? "Showtimes to be confirmed" }}</h3>
 
-      @if($film->venue->id != 13)
-        <p>Please check back again soon for details of specific showtimes and to book tickets.</p>
-        @if(config('app.mailchimp'))
-          <p>Full listings are confirmed every Monday for the week beginning the following Friday. To receive our weekly listings emails, <a href="{{ config('app.mailchimp') }}">sign up here.</a></p>
-        @endif
-      @endif
-    </div>
+    @if($film->venue->id != 13)
+    <p>
+      Please check back again soon for details of specific showtimes and to book
+      tickets.
+    </p>
+
+    @endif
+  </div>
   @endif
 </div>
--- resources/views/listings/weekly.blade.php
+++ resources/views/listings/weekly.blade.php
@@ -1,51 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section('content')
+<section class="section section--az-listings">
+  <h2 class="section-title">What’s On</h2>
+  @include('listings.navigation', ['type' => 'weekly'])
 
-  <section class="section section--az-listings">
-
-    <h2 class="section-title">What’s On</h2>
-    @include('listings.navigation', ['type' => 'weekly'])
-
-    <div class="weekly-screenings--content">
-
-      <div class="weekly-screenings--screenings container">
-        @if( isset($screenings_today) && count($screenings_today))
-          <h2 class="weekly-screenings--date">Today</h2>
-          <div class="weekly-screenings--entries">
-            @foreach ($screenings_today as $screening)
-              @include('screening.weekly-item')
-            @endforeach
-          </div>
-        @endif
-        @if(count($screenings))
-          @php $current_date = '' @endphp
-          @foreach ($screenings as $screening)
-            @if($screening->film)
-              @if ($current_date != $screening->date)
-                @if(!$loop->first)
-                  </div>
-                @endif
-                <h2 class="weekly-screenings--date">{{ Carbon\Carbon::parse($screening->date)->format('D jS F') }}</h2>
-                @php $current_date = $screening->date @endphp
-                <div class="weekly-screenings--entries">
-              @endif
-              @include('screening.weekly-item')
-            @endif
-          @endforeach
-          </div>
-        @if ($week > 1)
-          <div class="alert">
-            Full listings are confirmed every Monday for the week beginning the following Friday. To receive weekly listings as soon as they’re confirmed, <a href="{{ config('app.mailchimp') }}" target="_blank">sign up here.</a>
-          </div>
-        @endif
-        @else
-          <div class="alert alert__empty">No screenings currently scheduled for this week.</div>
-        @endif
-
-
+  <div class="weekly-screenings--content">
+    <div class="container weekly-screenings--screenings">
+      @if( isset($screenings_today) && count($screenings_today))
+      <h2 class="weekly-screenings--date">Today</h2>
+      <div class="weekly-screenings--entries">
+        @foreach ($screenings_today as $screening)
+        @include('screening.weekly-item') @endforeach
+      </div>
+      @endif @if(count($screenings)) @php $current_date = '' @endphp @foreach
+      ($screenings as $screening) @if($screening->film) @if ($current_date !=
+      $screening->date) @if(!$loop->first)
     </div>
-    @include('listings.navigation-footer')
-
+    @endif
+    <h2 class="weekly-screenings--date">
+      {{ Carbon\Carbon::parse($screening->date)->format('D jS F') }}
+    </h2>
+    @php $current_date = $screening->date @endphp
+    <div class="weekly-screenings--entries">
+      @endif @include('screening.weekly-item') @endif @endforeach
+    </div>
+    @if ($week > 1)
+    <div class="alert">
+      Full listings are confirmed every Monday for the week beginning the
+      following Friday.
+    </div>
+    @endif @else
+    <div class="alert alert__empty">
+      No screenings currently scheduled for this week.
+    </div>
+    @endif
+  </div>
+  @include('listings.navigation-footer')
 </section>
 @stop
